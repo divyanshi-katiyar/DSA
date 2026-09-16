6a1e98341304dc72bff5d92fe4eabc2db1429b84
Divyanshi Katiyar <divi.katiyar0435@gmail.com>
2026-09-16 12:18:45 +0530

    Time: 1 ms (10.72%) | Memory: 8 MB (56.30%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -6,11 +6,8 @@ public:
         }
         int n=s.length();
         //Try every possible combination
-        for(int count=1;count<=n;count++){
-            rotate(s.begin(),s.begin()+1,s.end());
-            if(s==goal){
-                return true;
-            }
+        if((s+s).find(goal)!=string::npos){
+            return true;
         }
         return false;
     }
